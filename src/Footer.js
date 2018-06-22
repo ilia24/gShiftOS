f6a7c68c58cb86508bb171dac81d283aec7e42b4
ilia24 <ilia.demertchian@gshiftlabs.com>
2018-06-22 12:58:45 -0400

    Add new font!

--- src/Footer.js
+++ src/Footer.js
@@ -7,8 +7,8 @@ class Footer extends Component {
       <div className="Footer">
         <div className="bottom_color_break"></div>
         <div className="title_box">
-          <img src={require('./assets/images/gshift-logo-retro.png')} alt="Gshift retro logo" />
-          <p className="fontTest">Entertainment System</p>
+          <p className="company_name"><span>g</span>Shift</p>
+          <p className="system_text">Entertainment System</p>
         </div>
       </div>
     );
