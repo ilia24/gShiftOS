322eff8f1fbfaa02dc203c0f5a962742a0d4e505
ilia24 <ilia.demertchian@gshiftlabs.com>
2018-06-25 09:29:30 -0400

    Add name display

---
+++ src/NameDisplay.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './NameDisplay.css';
+
+
+class NameDisplay extends Component {
+  render() {
+    return (
+      <div className="NameDisplay">
+        <p> John Appleseed</p>
+      </div>
+    );
+  }
+}
+
+export default NameDisplay;
--- src/Stage.js
+++ src/Stage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './Stage.css';
+import NameDisplay from './NameDisplay.js';
 
 class Stage extends Component {
   render() {
     return (
       <div className="Stage">
+        <NameDisplay />
       </div>
     );
   }
